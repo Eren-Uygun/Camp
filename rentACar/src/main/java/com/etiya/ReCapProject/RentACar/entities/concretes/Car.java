5d9ca2e6d1d3970fdd9eb2543225980417900406
Eren Uyğun <eren.uygun.2657@gmail.com>
2021-12-01 17:54:04 +0300

    RentACarProject 1.12.2021
    12-13-14 bitti

---
+++ rentACar/src/main/java/com/etiya/ReCapProject/RentACar/entities/concretes/Car.java
@@ -0,0 +1,40 @@
+package com.etiya.ReCapProject.RentACar.entities.concretes;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "cars")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "daily_price")
+    private double dailyPrice;
+
+    @Column(name = "model_year")
+    private int modelYear;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "findex_score")
+    private int findexScore;
+
+    @ManyToOne
+    @JoinColumn(name = "brand_id")
+    private Brand brand;
+
+    @ManyToOne
+    @JoinColumn(name = "color_id")
+    private Color color;
+}
