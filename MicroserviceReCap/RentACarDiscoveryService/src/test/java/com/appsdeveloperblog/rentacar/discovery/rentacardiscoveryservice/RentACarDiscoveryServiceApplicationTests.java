c8d1965fbe5f45b91f4fe0ddfc11b0e2394891f7
Eren Uyğun <eren.uygun.2657@gmail.com>
2021-12-08 17:59:35 +0300

    Rentals Entegrasyonu

---
+++ MicroserviceReCap/RentACarDiscoveryService/src/test/java/com/appsdeveloperblog/rentacar/discovery/rentacardiscoveryservice/RentACarDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appsdeveloperblog.rentacar.discovery.rentacardiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
