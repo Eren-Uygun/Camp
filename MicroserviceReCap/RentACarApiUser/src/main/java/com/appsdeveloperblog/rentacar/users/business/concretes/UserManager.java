74cf676020404e74bf7e8af9c60589a0a6c3ce3e
Eren Uyğun <eren.uygun.2657@gmail.com>
2021-12-10 12:52:53 +0300

    Login Security Operations

--- MicroserviceReCap/RentACarApiUser/src/main/java/com/appsdeveloperblog/rentacar/users/business/concretes/UserManager.java
+++ MicroserviceReCap/RentACarApiUser/src/main/java/com/appsdeveloperblog/rentacar/users/business/concretes/UserManager.java
@@ -1,23 +1,30 @@
 package com.appsdeveloperblog.rentacar.users.business.concretes;
 
+import java.util.ArrayList;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.appsdeveloperblog.rentacar.users.business.abstracts.UserService;
+import com.appsdeveloperblog.rentacar.users.business.dtos.UserDtos.UserDto;
 import com.appsdeveloperblog.rentacar.users.business.requests.UserRequests.LoginUserRequest;
 import com.appsdeveloperblog.rentacar.users.core.utils.mapping.ModelMapperService;
 import com.appsdeveloperblog.rentacar.users.core.utils.results.ErrorResult;
 import com.appsdeveloperblog.rentacar.users.core.utils.results.Result;
 import com.appsdeveloperblog.rentacar.users.core.utils.results.SuccessResult;
 import com.appsdeveloperblog.rentacar.users.dataAccess.UsersDao;
+import com.appsdeveloperblog.rentacar.users.entities.User;
 
 @Service
 public class UserManager implements UserService {
 	
 	private UsersDao usersDao;
 
+	@Autowired
 	public UserManager(UsersDao usersDao) {
-		super();
 		this.usersDao = usersDao;
 	}
 
@@ -66,6 +73,27 @@ public class UserManager implements UserService {
 			return new ErrorResult("Kullanıcı bulunamadı.");
 		}
 		return new SuccessResult();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = this.usersDao.getByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("Kullanıcı bulunamadı.");
+		}
+		
+		return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), true, true, true, true,
+				new ArrayList<>());
+	}
+
+	@Override
+	public UserDto getUserDetailsByEmail(String email) {
+		User user = this.usersDao.getByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("Kullanıcı bulunamadı.");
+		}
+		
+		return new ModelMapper().map(user, UserDto.class);
 	}
 
 }
