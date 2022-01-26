ab327052b6ace85a982e7cbd51b31c46225807b7
Eren Uyğun <eren.uygun.2657@gmail.com>
2022-01-26 14:45:42 +0300

    ReCapProject2

---
+++ WoodWorldBeta/src/main/java/com/recapProject/WoodWorldBeta/WoodWorldBetaApplication.java
@@ -0,0 +1,13 @@
+package com.recapProject.WoodWorldBeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoodWorldBetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WoodWorldBetaApplication.class, args);
+	}
+
+}
---
+++ WoodWorldBeta/src/test/java/com/recapProject/WoodWorldBeta/WoodWorldBetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.recapProject.WoodWorldBeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoodWorldBetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
