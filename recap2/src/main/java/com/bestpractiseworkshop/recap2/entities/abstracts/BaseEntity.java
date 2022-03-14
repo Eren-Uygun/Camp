e9f1ae7995811a4e13cda286efed2fd6d36a9344
Eren Uyğun <eren.uygun.2657@gmail.com>
2022-03-14 17:30:52 +0300

    Getter Setter

--- recap2/src/main/java/com/bestpractiseworkshop/recap2/entities/abstracts/BaseEntity.java
+++ recap2/src/main/java/com/bestpractiseworkshop/recap2/entities/abstracts/BaseEntity.java
@@ -15,4 +15,20 @@ public abstract class BaseEntity {
     private LocalDateTime uDate;
     private boolean isActv;
     
+    public LocalDateTime getcDate() {
+        return cDate;
+    }
+
+    public void setcDate(LocalDateTime cDate) {
+        cDate = LocalDateTime.now();
+        this.cDate = cDate;
+    }
+
+    public void setActv(boolean isActv) {
+        isActv = true;
+        this.isActv = isActv;
+    }
 }
+
+//Status için ayrı bi class sistemi yazılabilir ? 
+// id,status, vs....
