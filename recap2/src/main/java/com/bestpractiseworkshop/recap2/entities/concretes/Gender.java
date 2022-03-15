a15d24d45f27b1d415f8efcbd187e99638de728c
Eren Uyğun <eren.uygun.2657@gmail.com>
2022-03-15 17:22:51 +0300

    Gender + IndCust

---
+++ recap2/src/main/java/com/bestpractiseworkshop/recap2/entities/concretes/Gender.java
@@ -0,0 +1,26 @@
+package com.bestpractiseworkshop.recap2.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="genders")
+public class Gender {
+    
+    @Id
+    @Column(name="id")
+    private String genderId;
+    
+    @Column(name="gender_name")
+    private String genderName;
+
+    @Column(name="gender_description")
+    private String genderDescription;
+}
--- recap2/src/main/java/com/bestpractiseworkshop/recap2/entities/concretes/IndCustomer.java
+++ recap2/src/main/java/com/bestpractiseworkshop/recap2/entities/concretes/IndCustomer.java
@@ -1,5 +1,7 @@
 package com.bestpractiseworkshop.recap2.entities.concretes;
 
+import java.time.LocalDate;
+
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -13,5 +15,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "individual_customer")
 public class IndCustomer extends Customer {
+
+    private String firstName;
+    private String secondName;
+    private String lastName;
+    private String nationalIdentityNumber;
+    private LocalDate birthDate;
+    private String birthPlace;
+    
+     
     
 }
